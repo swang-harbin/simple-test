dddb985b2761ba28e6fcf3023d0cadb515645510
wangshuo <337229941@qq.com>
2021-06-04 09:44:11 +0800

    🧱chore: 添加SpringBoot框架

---
+++ src/main/java/icu/intelli/simpletest/SimpleTestApplication.java
@@ -0,0 +1,18 @@
+package icu.intelli.simpletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动类
+ *
+ * @author wangshuo
+ * @date 2021/06/04
+ */
+@SpringBootApplication
+public class SimpleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTestApplication.class);
+    }
+}
---
+++ src/test/java/icu/intelli/simpletest/SimpleTestTestApplication.java
@@ -0,0 +1,17 @@
+package icu.intelli.simpletest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * SpringBoot测试启动类
+ *
+ * @author wangshuo
+ * @date 2021/06/04
+ */
+@SpringBootTest
+public class SimpleTestTestApplication {
+
+    void test() {
+
+    }
+}
